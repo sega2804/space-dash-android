64ffea1fb45eeb2d216ee9b64664529120364635
SAMSARA <playmonoiseiga@gmail.com>
2026-01-20 09:22:17 +0100

    feat: Add haptic feedback to all UI buttons

--- app/src/main/java/com/crypticsamsara/spacedash/MainActivity.kt
+++ app/src/main/java/com/crypticsamsara/spacedash/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.crypticsamsara.spacedash.ui.audio.SoundManager
+import com.crypticsamsara.spacedash.ui.haptics.HapticManager
 import com.crypticsamsara.spacedash.ui.screens.GameNavigation
 import com.crypticsamsara.spacedash.ui.screens.GameScreen
 import com.crypticsamsara.spacedash.ui.theme.SpaceDashTheme
@@ -24,13 +25,15 @@ import com.crypticsamsara.spacedash.viewmodel.GameViewModel
 class MainActivity : ComponentActivity() {
 
     private lateinit var soundManager: SoundManager
+    private lateinit var hapticManager: HapticManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         // Start soundManager
-        soundManager = SoundManager(this)
+        soundManager = SoundManager(this),
+        hapticManager = HapticManager(this)
 
         setContent {
             SpaceDashTheme {
@@ -40,7 +43,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     // create viewmodel with soundManager
                     val viewModel: GameViewModel = viewModel(
-                        factory = GameViewModelFactory(soundManager)
+                        factory = GameViewModelFactory(soundManager, hapticManager)
                     )
                     GameScreen(viewModel = viewModel)
                 }
@@ -60,6 +63,7 @@ class MainActivity : ComponentActivity() {
     override fun onDestroy() {
         super.onDestroy()
         soundManager.release()
+        hapticManager.cancel()
     }
 }
 
